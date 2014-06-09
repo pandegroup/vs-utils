de31f5cf7e69894aa8c80ff08e4f953033e7c753
skearnes <kearnes@stanford.edu>
2014-06-09 15:10:58 -0700

    int -> intc to avoid platform issues

--- pande_gas/utils/rdkit_utils.py
+++ pande_gas/utils/rdkit_utils.py
@@ -128,7 +128,7 @@ def generate_conformers(mol, n_conformers=1, rmsd_threshold=0.5):
     cids = AllChem.EmbedMultipleConfs(mol, n_conformers,
                                       pruneRmsThresh=rmsd_threshold)
     assert mol.GetNumConformers() >= 1
-    cids = np.asarray(cids, dtype=int)
+    cids = np.asarray(cids, dtype='intc')
 
     # minimize conformers and get energies
     energy = np.zeros(cids.size, dtype=float)
