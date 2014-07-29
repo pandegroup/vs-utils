59c6ef60c1e8b2ceb3ac3b0031937be227457f9d
skearnes <kearnes@stanford.edu>
2014-07-29 15:09:19 -0700

    Add tests for hydrogen removal

--- pande_gas/features/tests/test_coulomb_matrices.py
+++ pande_gas/features/tests/test_coulomb_matrices.py
@@ -25,15 +25,41 @@ class TestCoulombMatrix(unittest.TestCase):
         assert self.mol.GetNumConformers() > 0
 
     def test_coulomb_matrix(self):
-        """Test CoulombMatrix."""
+        """
+        Test CoulombMatrix.
+        """
         f = cm.CoulombMatrix(self.mol.GetNumAtoms())
         rval = f([self.mol])
         size = np.triu_indices(self.mol.GetNumAtoms())[0].size
         assert rval.shape == (1, self.mol.GetNumConformers(), size)
 
     def test_coulomb_matrix_padding(self):
-        """Test CoulombMatrix with padding."""
+        """
+        Test CoulombMatrix with padding.
+        """
         f = cm.CoulombMatrix(max_atoms=self.mol.GetNumAtoms() * 2)
         rval = f([self.mol])
         size = np.triu_indices(self.mol.GetNumAtoms() * 2)[0].size
+        assert rval.shape == (1, self.mol.GetNumConformers(), size)
+
+    def test_coulomb_matrix_no_hydrogens(self):
+        """
+        Test hydrogen removal.
+        """
+        mol = Chem.RemoveHs(self.mol)
+        assert mol.GetNumAtoms() < self.mol.GetNumAtoms()
+        f = cm.CoulombMatrix(max_atoms=mol.GetNumAtoms(),
+                             remove_hydrogens=True)
+        rval = f([self.mol])  # use the version with hydrogens
+        size = np.triu_indices(mol.GetNumAtoms())[0].size
+        assert rval.shape == (1, mol.GetNumConformers(), size)
+
+    def test_coulomb_matrix_hydrogens(self):
+        """
+        Test no hydrogen removal.
+        """
+        f = cm.CoulombMatrix(max_atoms=self.mol.GetNumAtoms(),
+                             remove_hydrogens=False)
+        rval = f([self.mol])
+        size = np.triu_indices(self.mol.GetNumAtoms())[0].size
         assert rval.shape == (1, self.mol.GetNumConformers(), size)
