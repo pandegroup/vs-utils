6e587c6855aeb16f18386ea20f67fba49ec7f364
skearnes <kearnes@stanford.edu>
2014-09-03 13:29:27 -0700

    Add RDKit descriptors

--- pande_gas/features/basic.py
+++ pande_gas/features/basic.py
@@ -6,7 +6,7 @@ __author__ = "Steven Kearnes"
 __copyright__ = "Copyright 2014, Stanford University"
 __license__ = "BSD 3-clause"
 
-from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import Descriptors, rdMolDescriptors
 
 from pande_gas.features import Featurizer
 
@@ -26,6 +26,37 @@ class MolecularWeight(Featurizer):
         mol : RDKit Mol
             Molecule.
         """
-        wt = rdMolDescriptors.CalcExactMolWt(mol)
+        wt = Descriptors.ExactMolWt(mol)
         wt = [wt]
         return wt
+
+
+class SimpleDescriptors(Featurizer):
+    """
+    RDKit descriptors.
+
+    See http://rdkit.org/docs/GettingStartedInPython.html
+    #list-of-available-descriptors.
+    """
+    name = 'descriptors'
+
+    def __init__(self):
+        self.descriptors = []
+        self.functions = []
+        for descriptor, function in Descriptors.descList:
+            self.descriptors.append(descriptor)
+            self.functions.append(function)
+
+    def _featurize(self, mol):
+        """
+        Calculate RDKit descriptors.
+
+        Parameters
+        ----------
+        mol : RDKit Mol
+            Molecule.
+        """
+        descriptors = []
+        for function in self.functions:
+            descriptors.append(function(mol))
+        return descriptors
