35fa384cc484ba846f1c1bbdc69955a5806c4f10
skearnes <kearnes@stanford.edu>
2014-10-01 12:23:47 -0700

    Add get_smiles_map.py

---
+++ pande_gas/scripts/get_smiles_map.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+Get SMILES for compounds and map to compound names.
+"""
+
+__author__ = "Steven Kearnes"
+__copyright__ = "Copyright 2014, Stanford University"
+__license__ = "BSD 3-clause"
+
+import argparse
+import cPickle
+import gzip
+
+from rdkit_utils import serial
+
+from ..utils import SmilesMap
+
+
+def parse_args(input_args=None):
+    """
+    Parse command-line arguments.
+
+    Parameters
+    ----------
+    input_args : list, optional
+        Input arguments. If not provided, defaults to sys.argv[1:].
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required=1,
+                        help='Input molecule filename.')
+    parser.add_argument('-o', '--output', required=1,
+                        help='Output filename.')
+    parser.add_argument('-p', '--prefix',
+                        help='Prefix to prepend to molecule IDs.')
+    return parser.parse_args(input_args)
+
+
+def main(input_filename, output_filename, id_prefix=None):
+    """
+    Get SMILES for compounds and map to compound names.
+
+    Parameters
+    ----------
+    input_filename : str
+        Input molecule filename.
+    output_filename : str
+        Output filename.
+    id_prefix : str, optional
+        Prefix to prepend to IDs.
+    """
+    smiles = SmilesMap(id_prefix)
+    with serial.MolReader().open(input_filename) as reader:
+        for mol in reader:
+            smiles.add_mol(mol)
+    if output_filename.endswith('.gz'):
+        f = gzip.open(output_filename, 'wb')
+    else:
+        f = open(output_filename, 'wb')
+    cPickle.dump(smiles.get_map(), f, cPickle.HIGHEST_PROTOCOL)
+    f.close()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.input, args.output, args.prefix)
