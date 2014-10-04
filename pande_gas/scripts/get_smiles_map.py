9f47cdf631b8584d1239a752164c493c9b679c26
skearnes <kearnes@stanford.edu>
2014-10-03 17:00:15 -0700

    Add allow_duplicates keyword

--- pande_gas/scripts/get_smiles_map.py
+++ pande_gas/scripts/get_smiles_map.py
@@ -32,12 +32,16 @@ def parse_args(input_args=None):
                         help='Output filename.')
     parser.add_argument('-p', '--prefix',
                         help='Prefix to prepend to molecule IDs.')
+    parser.add_argument('--no-duplicates', action='store_false',
+                        dest='allow_duplicates',
+                        help='Whether to allow duplicate SMILES.')
     parser.add_argument('-u', '--update', action='store_true',
                         help='Update existing map with same output filename.')
     return parser.parse_args(input_args)
 
 
-def main(input_filenames, output_filename, id_prefix=None, update=False):
+def main(input_filenames, output_filename, id_prefix=None,
+         allow_duplicates=True, update=False):
     """
     Get SMILES for compounds and map to compound names.
 
@@ -49,11 +53,13 @@ def main(input_filenames, output_filename, id_prefix=None, update=False):
         Output filename.
     id_prefix : str, optional
         Prefix to prepend to IDs.
+    allow_duplicates : bool, optional (default True)
+        Whether to allow duplicate SMILES.
     update : bool, optional (default False)
         Whether to update an existing map with the same output filename. If
         False, a new map will be generated using only the input file(s).
     """
-    smiles = SmilesMap(id_prefix)
+    smiles = SmilesMap(prefix=id_prefix, allow_duplicates=allow_duplicates)
 
     # update existing map
     if update:
@@ -79,4 +85,5 @@ def main(input_filenames, output_filename, id_prefix=None, update=False):
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.input, args.output, args.prefix, args.update)
+    main(args.input, args.output, args.prefix, args.allow_duplicates,
+         args.update)
