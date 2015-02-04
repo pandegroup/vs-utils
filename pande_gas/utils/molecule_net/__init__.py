bea4c5a1f207952195fa606a410b5783cbcd7cae
skearnes <kearnes@stanford.edu>
2015-02-04 13:50:59 -0800

    Rename class and fill out get_target

--- pande_gas/utils/molecule_net/__init__.py
+++ pande_gas/utils/molecule_net/__init__.py
@@ -1,10 +1,11 @@
 """
 Utilities for MoleculeNet.
 """
+import re
 import xml.etree.cElementTree as et
 
 
-class AssayXMLParser(object):
+class PcbaXmlParser(object):
     """
     Parser for PubChem BioAssay XML.
 
@@ -20,7 +21,7 @@ class AssayXMLParser(object):
         # default prefix for all tags
         self.prefix = '{http://www.ncbi.nlm.nih.gov}'
 
-    def find(self, tag):
+    def find(self, tag, root=None):
         """
         Return a list of the elements with a given tag.
 
@@ -28,8 +29,27 @@ class AssayXMLParser(object):
         ----------
         tag : str
             XML tag.
+        root : bool, optional (default False)
+            Root of XML tree.
         """
-        return self.root.findall(self.prefix + tag)
+        if root is None:
+            root = self.root
+        return root.findall(self.prefix + tag)
+
+    def join_children(self, elem):
+        """
+        Join the text for the children of an element.
+
+        Parameters
+        ----------
+        elem : Element
+            Element.
+        """
+        text = ''
+        for child in elem.getchildren():
+            if child.text is not None:
+                text += child.text + child.tail
+        return text
 
     def get_name(self):
         """
@@ -45,18 +65,35 @@ class AssayXMLParser(object):
         """
         elem = self.find('PC-AssayDescription_description')
         assert len(elem) == 1
-        description = ''
-        for child in elem[0].getchildren():
-            if child.text is not None:
-                description += child.text + child.tail
-        return description
+        return self.join_children(elem[0])
 
     def get_protocol(self):
         """
         Get assay protocol.
         """
+        elem = self.find('PC-AssayDescription_protocol')
+        assert len(elem) == 1
+        return self.join_children(elem[0])
 
     def get_target(self):
         """
         Get assay target.
+
+        Returns
+        -------
+        target : dict
+            A dictionary containing keys for target information types, such
+            as 'name', 'mol-id', and 'molecule-type'.
         """
+        elem = self.find('PC-AssayDescription_target')
+        assert len(elem) == 1
+        info = self.find('PC-AssayTargetInfo', elem[0])
+        assert len(info) == 1
+        target = {}
+        for e in info[0].getchildren():
+            if not e.text.strip():
+                continue  # skip blank entries
+            m = re.search('PC-AssayTargetInfo_(.*)', e.tag)
+            key = m.groups()[0]
+            target[key] = e.text
+        return target
