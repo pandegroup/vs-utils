1d746476cd270d0df8c373d6013077d3505461e1
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2015-02-06 13:04:16 -0800

    Added output directory

--- pande_gas/scripts/molecule_net/download_pcba_descriptions.py
+++ pande_gas/scripts/molecule_net/download_pcba_descriptions.py
@@ -2,6 +2,7 @@
 """
 Download PCBA assay descriptions.
 """
+import os
 import argparse
 
 from pubchem_utils import PubChem
@@ -21,10 +22,13 @@ def parse_args(input_args=None):
                         help='Input file containing AIDs.')
     parser.add_argument('-f', '--format', choices=['json', 'xml'],
                         required=True)
+    parser.add_argument('--out',
+                        help="Output directory for generated files.",
+                        required=True)
     return parser.parse_args(input_args)
 
 
-def main(filename, output_format):
+def main(filename, output_format, output_dir):
     """
     Download PCBA JSON descriptions.
 
@@ -32,17 +36,23 @@ def main(filename, output_format):
     ----------
     filename : str
         Filename containing AIDs.
-    output_format : str, optional (default 'json')
+    output_format : str (default 'json')
         Output file format.
+    output_dir : str
+        Output directory where downloaded files will be written.
     """
+    if not os.path.isdir(output_dir):
+      raise ValueError("%s is not a valid output directory!" % output_dir)
     with open(filename) as f:
-        aids = [int(line) for line in f]
+      aids = [int(line) for line in f]
     engine = PubChem()
     for aid in aids:
-        data = engine.get_assay_description(aid, output_format)
-        with open('aid{}.{}'.format(aid, output_format), 'wb') as f:
-            f.write(data)
+      out_file = os.path.join(
+          output_dir, 'aid{}.{}'.format(aid, output_format))
+      data = engine.get_assay_description(aid, output_format)
+      with open(out_file, 'wb') as f:
+          f.write(data)
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.input, args.format)
+    main(args.input, args.format, args.out)
