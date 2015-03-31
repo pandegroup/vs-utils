f89942f6e95eab4e52ff6f036251aebace44186d
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2015-03-31 15:56:01 -0700

    More 2-space adjustments.

--- vs_utils/features/basic.py
+++ vs_utils/features/basic.py
@@ -12,51 +12,51 @@ from vs_utils.features import Featurizer
 
 
 class MolecularWeight(Featurizer):
-    """
-    Molecular weight.
-    """
-    name = ['mw', 'molecular_weight']
+  """
+  Molecular weight.
+  """
+  name = ['mw', 'molecular_weight']
 
-    def _featurize(self, mol):
-        """
-        Calculate molecular weight.
+  def _featurize(self, mol):
+    """
+    Calculate molecular weight.
 
-        Parameters
-        ----------
-        mol : RDKit Mol
-            Molecule.
-        """
-        wt = Descriptors.ExactMolWt(mol)
-        wt = [wt]
-        return wt
+    Parameters
+    ----------
+    mol : RDKit Mol
+        Molecule.
+    """
+    wt = Descriptors.ExactMolWt(mol)
+    wt = [wt]
+    return wt
 
 
 class SimpleDescriptors(Featurizer):
+  """
+  RDKit descriptors.
+
+  See http://rdkit.org/docs/GettingStartedInPython.html
+  #list-of-available-descriptors.
+  """
+  name = 'descriptors'
+
+  def __init__(self):
+    self.descriptors = []
+    self.functions = []
+    for descriptor, function in Descriptors.descList:
+        self.descriptors.append(descriptor)
+        self.functions.append(function)
+
+  def _featurize(self, mol):
     """
-    RDKit descriptors.
+    Calculate RDKit descriptors.
 
-    See http://rdkit.org/docs/GettingStartedInPython.html
-    #list-of-available-descriptors.
+    Parameters
+    ----------
+    mol : RDKit Mol
+        Molecule.
     """
-    name = 'descriptors'
-
-    def __init__(self):
-        self.descriptors = []
-        self.functions = []
-        for descriptor, function in Descriptors.descList:
-            self.descriptors.append(descriptor)
-            self.functions.append(function)
-
-    def _featurize(self, mol):
-        """
-        Calculate RDKit descriptors.
-
-        Parameters
-        ----------
-        mol : RDKit Mol
-            Molecule.
-        """
-        rval = []
-        for function in self.functions:
-            rval.append(function(mol))
-        return rval
+    rval = []
+    for function in self.functions:
+        rval.append(function(mol))
+    return rval
