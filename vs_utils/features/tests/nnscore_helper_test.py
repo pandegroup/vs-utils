212446a18d9964e7ec725e71ced8517222f316e3
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2015-03-31 17:10:00 -0700

    Moving MathFunctions over to nnscore_helper_test.

--- vs_utils/features/tests/nnscore_helper_test.py
+++ vs_utils/features/tests/nnscore_helper_test.py
@@ -1,26 +1,28 @@
 """
 Test NNScore Helper Classes
 """
+import tempfile
+import shutil
 import unittest
 import numpy as np
 
-from vs_utils.features.nnscore_helper import point, atom
+from vs_utils.features.nnscore_helper import point, atom, PDB
 
 
 class TestPoint(unittest.TestCase):
   """
-  Test point.
+  Test point class.
   """
   def setUp(self):
     """
-    Instantiate a local point object.
+    Instantiate local points for tests.
     """
     self.point_a = point(1,2,3)
     self.point_b = point(-1,-2,-3)
 
   def testCopyOf(self):
     """
-    Verify that copy_of copies x,y,z correctly.
+    TestPoint: Verify that copy_of copies x,y,z correctly.
     """
     copy_point = self.point_a.copy_of()
     assert copy_point.x == 1
@@ -29,7 +31,7 @@ class TestPoint(unittest.TestCase):
 
   def testDistTo(self):
     """
-    Verify that dist_to implements L2-distance.
+    TestPoint: Verify that dist_to implements L2-distance.
     """
     ## || point_a - point_b ||_2 = ||(1,2,3) - (-1,-2,-3)||_2
     ##                           = ||(2,4,6)||_2
@@ -39,7 +41,7 @@ class TestPoint(unittest.TestCase):
 
   def testMagnitude(self):
     """
-    Verify that magnitude implements L2-Norm.
+    TestPoint: Verify that magnitude implements L2-Norm.
     """
     ## || (1, 2, 3) ||_2 = || (-1, -2, -3) ||_2
     ##                   = sqrt(1 + 4 + 9)
@@ -49,9 +51,96 @@ class TestPoint(unittest.TestCase):
 
   def testCreatePDBLine(self):
     """
-    Verify that PDB Line is in correct format.
+    TestPoint: Verify that PDB Line is in correct format.
     """
     # TODO(bramsundar): Add a more nontrivial test after looking into
     # PDB standard.
     line = self.point_a.CreatePDBLine(1)
     assert type(line) == str
+
+class TestAtom(unittest.TestCase):
+  """
+  Test atom class.
+  """
+
+  def setUp(self):
+    """
+    Instantiates a pair of atom objects for tests.
+    """
+    self.empty_atom = atom()
+    self.trial_atom = atom()
+    self.trial_atom.atomname = "C"
+    self.trial_atom.coordinates = point(1,2,3)
+    self.trial_atom.charge = 0.
+    self.trial_atom.element = "C"
+    self.trial_atom.residue = "CYS"
+    # TODO(bramsundar): Fill in a non-junk value for chain.
+    self.trial_atom.chain = "FF"
+    self.trial_atom.IndicesOfAtomsConnecting = [4, 5, 6]
+
+  def testCopyOf(self):
+    """
+    TestAtom: Verify that copy_of preserves atom information.
+    """
+    copy_atom = self.trial_atom.copy_of()
+    assert copy_atom.atomname == "C"
+    assert copy_atom.coordinates.x == 1
+    assert copy_atom.coordinates.y == 2
+    assert copy_atom.coordinates.z == 3
+    assert copy_atom.charge == 0
+    assert copy_atom.element == "C"
+    assert copy_atom.residue == "CYS"
+    assert copy_atom.chain == "FF"
+    assert copy_atom.IndicesOfAtomsConnecting == [4, 5, 6]
+
+  def testCreatePDBLine(self):
+    """
+    TestAtom: Verify that PDB Line is in correct format.
+    """
+    # TODO(bramsundar): Add a more nontrivial test after looking into
+    # PDB standard.
+    line = self.trial_atom.CreatePDBLine(1)
+    assert type(line) == str
+
+  def testNumberOfNeighors(self):
+    """
+    TestAtom: Verify that the number of neighbors is computed correctly.
+    """
+    assert self.empty_atom.NumberOfNeighbors() == 0
+    assert self.trial_atom.NumberOfNeighbors() == 3
+
+  # TODO(bramsundar): Add more tests here.
+
+class TestPDB(unittest.TestCase):
+  """"
+  Test PDB class.
+  """
+
+  def setUp(self):
+    """
+    Instantiate a dummy PDB file.
+    """
+    self.temp_dir = tempfile.mkdtemp()
+    self.pdb = PDB()
+
+    _, self.pdb_filename = tempfile.mkstemp(suffix='.pdb', 
+        dir=self.temp_dir)
+
+  def tearDown(self):
+    """
+    Delete temporary directory.
+    """
+    shutil.rmtree(self.temp_dir)
+
+  def testSaveAndLoad(self):
+    """
+    TestPDB: Saves dummpy PDB to file and verifies that it can be reloaded.
+    """
+    self.pdb.SavePDB(self.pdb_filename) 
+    empty_pdb = PDB()
+    with file.open(self.pdb_filename) as pdb_file:
+      for line in pdb_file:
+        print line
+    empty_pdb.LoadPDB_from_file(self.pdb_filename)
+    
+
