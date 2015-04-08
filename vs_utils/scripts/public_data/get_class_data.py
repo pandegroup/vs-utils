6d929366b1d4d615443dbf7619a0e69c01f87352
Steven Kearnes <skearnes@users.noreply.github.com>
2015-04-08 08:05:59 -0700

    More generic column names

--- vs_utils/scripts/public_data/get_class_data.py
+++ vs_utils/scripts/public_data/get_class_data.py
@@ -10,7 +10,7 @@ from vs_utils.utils.rdkit_utils import serial
 def parse_args(input_args=None):
   """Parse command-line arguments."""
   parser = argparse.ArgumentParser()
-  parser.add_argument('--aid', required=1,
+  parser.add_argument('--assay', required=1,
                       help='Assay ID.')
   parser.add_argument('--target', required=1,
                       help='Assay target.')
@@ -18,7 +18,7 @@ def parse_args(input_args=None):
                       help='File containing actives.')
   parser.add_argument('-d', '--decoys', required=1,
                       help='File containing decoys.')
-  parser.add_argument('--no-aid', action='store_false', dest='with_aid',
+  parser.add_argument('--no-assay', action='store_false', dest='with_assay',
                       help='Do not include AID with each data point.')
   parser.add_argument('--no-target', action='store_false', dest='with_target',
                       help='Do not include target with each data point.')
@@ -33,20 +33,20 @@ def parse_args(input_args=None):
   return parser.parse_args(input_args)
 
 
-def get_rows(reader, outcome, aid, target, with_aid=True, with_target=True,
+def get_rows(reader, outcome, assay, target, with_aid=True, with_target=True,
              phenotype=None, join=False):
   """Get a row for each data point."""
   rows = []
   cids = set()
   mols = set()
   for mol in reader:
-    row = {'cid': mol.GetProp('_Name'), 'outcome': outcome}
-    if join and row['cid'] in cids:
+    row = {'id': mol.GetProp('_Name'), 'outcome': outcome}
+    if join and row['id'] in cids:
       continue
-    cids.add(row['cid'])
+    cids.add(row['id'])
     mols.add(mol)
     if with_aid:
-      row['aid'] = aid
+      row['assay'] = assay
     if with_target:
       row['target'] = target
     if phenotype is not None:
@@ -55,7 +55,7 @@ def get_rows(reader, outcome, aid, target, with_aid=True, with_target=True,
   return rows, mols
 
 
-def main(active_filename, decoy_filename, aid, target, with_aid=True,
+def main(active_filename, decoy_filename, assay, target, with_assay=True,
          with_target=True, phenotype=None, join=False, output_filename=None,
          unique_filename=None):
   rows = []
@@ -66,8 +66,9 @@ def main(active_filename, decoy_filename, aid, target, with_aid=True,
     if outcome == 'inactive' and phenotype is not None:
       this_phenotype = 'inactive'
     with serial.MolReader().open(filename) as reader:
-      this_rows, this_mols = get_rows(reader, outcome, aid, target, with_aid,
-                                      with_target, this_phenotype, join)
+      this_rows, this_mols = get_rows(reader, outcome, assay, target,
+                                      with_assay, with_target, this_phenotype,
+                                      join)
       rows.extend(this_rows)
       mols.extend(this_mols)
   assert len(rows) == len(mols)
@@ -75,7 +76,7 @@ def main(active_filename, decoy_filename, aid, target, with_aid=True,
   df = pd.DataFrame(rows)
   if output_filename is None:
     output_filename = '{}-{}-data.pkl.gz'.format(aid, target)
-  print '{}\t{}\t{}\t{}'.format(aid, target, output_filename, len(df))
+  print '{}\t{}\t{}\t{}'.format(assay, target, output_filename, len(df))
   utils.write_pickle(df, output_filename)
 
   if unique_filename is not None:
@@ -84,5 +85,5 @@ def main(active_filename, decoy_filename, aid, target, with_aid=True,
 
 if __name__ == '__main__':
   args = parse_args()
-  main(args.actives, args.decoys, args.aid, args.target, args.with_aid,
+  main(args.actives, args.decoys, args.assay, args.target, args.with_assay,
        args.with_target, args.phenotype, args.join, args.output, args.mols)
